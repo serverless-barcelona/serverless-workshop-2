a98ebe2577f9577edd116611f66faf81a26f0890
Enric Soler Rastrollo <me@enricsoler.dev>
2020-01-30 23:31:22 +0100

    Add Firehost from GUI

--- build-config-file-and-sync-s3.js
+++ build-config-file-and-sync-s3.js
@@ -11,33 +11,35 @@ async function run(command) {
     console.log('stderr:', stderr);
 }
 
-function getOutputValue(OutputArray,OutputKey){
+function getOutputValue(OutputArray, OutputKey) {
     var ret = false;
     OutputArray.map(output => {
-        if(output.OutputKey == OutputKey) {
-        	ret = output.OutputValue;
-		} 
+        if (output.OutputKey == OutputKey) {
+            ret = output.OutputValue;
+        }
     })
     return ret;
 }
 
-(async() => {
+(async () => {
     const cloudformation = await run(command);
     console.log(cloudformation);
     const output = JSON.parse(cloudformation).Stacks[0].Outputs;
     console.log(output);
     const configFile = `window._config = {
         cognito: {
-            userPoolId: '${getOutputValue(output,'CognitoUserPoolId')}',
-            userPoolClientId: '${getOutputValue(output,'CognitoUserPoolClientId')}',
-            region: '${state.service.provider.region}'
+            userPoolId: '${getOutputValue(output, 'CognitoUserPoolId')}',
+            userPoolClientId: '${getOutputValue(output, 'CognitoUserPoolClientId')}',
+            userIdentityPoolId: '${getOutputValue(output, 'CognitoIdentityPoolId')}',
+            region: '${state.service.provider.region}',
+            deliveryStreamName: '${getOutputValue(output, 'DeliveryStreamName')}'
         },
         api: {
-            invokeUrl: '${getOutputValue(output,'ServiceEndpoint')}'
+            invokeUrl: '${getOutputValue(output, 'ServiceEndpoint')}'
         }
     };`;
-    fs.writeFileSync('./website/js/config.js',configFile);
-    const website = getOutputValue(output,'WebsiteBucket');
+    fs.writeFileSync('./website/js/config.js', configFile);
+    const website = getOutputValue(output, 'WebsiteBucket');
     await run(`aws s3 sync ./website s3://${website}`);
     console.log(configFile);
 })();
--- website/js/init.js
+++ website/js/init.js
@@ -1,37 +1,55 @@
-$(document).ready(()=>{
+$(document).ready(() => {
     var customEvents = window.customEvents;
-    $('#register').click(function(ev){
-        ev.preventDefault(); 
-        customEvents.emit('register',{
-            email:$('#emailInputRegister').val(),
-            password:$('#passwordInputRegister').val()
+
+    // Set Up Events
+    $('#register').click(function (ev) {
+        ev.preventDefault();
+        customEvents.emit('register', {
+            email: $('#emailInputRegister').val(),
+            password: $('#passwordInputRegister').val()
         })
     });
 
-    $('#confirm').click(function(ev){
-        ev.preventDefault(); 
-        customEvents.emit('confirm_user',{
+    $('#confirm').click(function (ev) {
+        ev.preventDefault();
+        customEvents.emit('confirm_user', {
             cognitoUser,
-            code:$('#code').val()
+            code: $('#code').val()
         })
     });
 
-    $('#signin').click(function(ev){
-        ev.preventDefault(); 
-        customEvents.emit('login',{
+    $('#signin').click(function (ev) {
+        ev.preventDefault();
+        customEvents.emit('login', {
             Username: $('#emailInputSignin').val(),
-            Password:$('#passwordInputSignin').val()
+            Password: $('#passwordInputSignin').val()
         });
     });
 
-    $('#openapi').click(function(ev){
-        ev.preventDefault(); 
+    $('#signout').click(function (ev) {
+        ev.preventDefault();
+        customEvents.emit('sign_out');
+    });
+
+    $('#openapi').click(function (ev) {
+        ev.preventDefault();
         req('openapi');
     });
-    
-    $('#privateapi').click(function(ev){
-        ev.preventDefault(); 
+
+    $('#privateapi').click(function (ev) {
+        ev.preventDefault();
         req('api');
     });
-    
+
+    $('#sendToFirehose').click((ev) => {
+        ev.preventDefault();
+        sendToFirehose({ a: 'b' });
+    });
+
+    document.addEventListener('click', () => {
+        console.log('I will listen to all clicks');
+    })
+
+    // Trigger Initialization Flow
+    customEvents.emit('init_ready');
 });
